fda930bf90207e17fccfaacddf5c4d803c60a392
Daniel Ancines <daniel.ancines@gmail.com>
2024-07-30 06:02:35 -0300

    Load items

--- src/app/modules/admin/example/example.component.ts
+++ src/app/modules/admin/example/example.component.ts
@@ -3,43 +3,52 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { catchError, map } from 'rxjs/operators';
+import {MatTableModule} from '@angular/material/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector     : 'example',
     standalone   : true,
     templateUrl  : './example.component.html',
     encapsulation: ViewEncapsulation.None,
-    imports: [MatButtonModule]
+    imports: [CommonModule, MatButtonModule, MatTableModule]
 })
 export class ExampleComponent implements OnInit
 {
-    products: any[];
+    products: IProduct[] = [];
+    displayedColumns: string[] = ['Name', 'Price'];
+
     constructor(private httpClient: HttpClient, private router: Router)
     {
         console.log('Constructor');
     }
 
     loadData(): void {
-        const headers = new HttpHeaders({
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRhbmllbC5hbmNpbmVzQGdtYWlsLmNvbSIsIm5iZiI6MTcyMTg5ODYwMiwiZXhwIjoxNzIxOTA1ODAyLCJpYXQiOjE3MjE4OTg2MDJ9.Y35KsVJMnBvdpaAMm49A3S-DROFN4psl5znsSGuLCus'
-        });
-
         const params = new HttpParams();
-        params.append("orderBy", "name");
+        params.set("orderBy", "name");
 
-        this.httpClient.get<any[]>('http://api.danielancines.com/api/v1/product', {headers: headers, params: params})
+        this.httpClient.get<any[]>('http://api.danielancines.com/api/v1/product?orderBy=name', {params: params})
         .pipe(
             catchError(error => {
                 console.log(error);
                 return [];
             }))
         .subscribe(data => {
-            this.products = data;
-            console.log(data);
+            data.forEach(product => {
+                this.products = this.products.concat({
+                    Name: product.name,
+                    Price: product.prices.pop().price
+                });
+            });
         });
     }
 
     ngOnInit(): void {
-
+        this.loadData();
     }
+}
+
+interface IProduct {
+    Name: string,
+    Price: number
 }
